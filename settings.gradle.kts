da320971ee9054a77e4291fe1315ef6950b471c3
srvenient <srvenient@gmail.com>
2024-10-10 23:53:08 -0500

    📦 Add and change some modules to improve code maintainability

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,18 @@ rootProject.name = "gui"
 sequenceOf(
   "common",
   "core",
-  "test-plugin"
+  "runtime-bukkit:api",
+  "runtime-bukkit:adapt:v1_21_1",
+  "runtime-bukkit:test-plugin"
 ).forEach {
-  include("${rootProject.name}-$it")
-  project(":${rootProject.name}-$it").projectDir = file(it)
+  includePrefixed(it)
+}
+
+fun includePrefixed(name: String) {
+  val kebabName = name.replace(":", "-")
+  val path = name.replace(":", "/")
+  val baseName = "${rootProject.name}-$kebabName"
+
+  include(baseName)
+  project(":$baseName").projectDir = file(path)
 }
