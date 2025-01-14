f013a19bc9b398752601b1f4dcf0c4459fbefc57
srvenient <srvenient@gmail.com>
2025-01-14 14:08:50 -0500

    🛠️ add as dependencies to the runtime-bukkit modules the placeholder api

--- build-logic/src/main/kotlin/gui.runtime-bukkit-conventions.gradle.kts
+++ build-logic/src/main/kotlin/gui.runtime-bukkit-conventions.gradle.kts
@@ -4,6 +4,11 @@ plugins {
 
 val libs = extensions.getByType(org.gradle.accessors.dm.LibrariesForLibs::class)
 
+repositories {
+  maven("https://repo.extendedclip.com/releases/")
+}
+
 dependencies {
   compileOnly(libs.paper)
+  compileOnly("me.clip:placeholderapi:2.11.6")
 }
